0679c70eb01b0c27fee65d50bd3e67576cf0f795
Shay Davidson <shay.h.davidson@gmail.com>
2012-12-26 18:53:58 +0200

    Added validation

--- app/models/state.rb
+++ app/models/state.rb
@@ -2,4 +2,7 @@ class State < ActiveRecord::Base
 
   attr_accessible :active, :available_bikes, :available_docks
 
+  validates_numericality_of :available_bikes, :greater_than_or_equal_to => 0
+  validates_numericality_of :available_docks, :greater_than_or_equal_to => 0
+
 end
