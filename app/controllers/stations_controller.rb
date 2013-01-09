99a6ee87189d3eb13d21b6ae34712784c6059dee
Shay Davidson <shay.h.davidson@gmail.com>
2013-01-09 16:25:36 +0200

    sync action

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -1,4 +1,5 @@
-#require './lib/services/telofun/station_serializer.rb'
+require './lib/services/telofun/api'
+require './lib/services/telofun/station_syncer'
 
 class StationsController < ApplicationController
 
@@ -22,6 +23,12 @@ class StationsController < ApplicationController
       :station => station
     }
     respond_with(@station_data)
+  end
+
+  def sync
+    raw_stations_data = Telofun::API.get_all_stations
+    Telofun::StationSyncer.sync(raw_stations_data)
+    render :nothing => true, :status => :ok
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Statofun::Application.routes.draw do
   root :to => "map#index"
 
   resources :stations, :only => [:show, :index] do
+    collection do
+      get 'sync'
+    end
   end
 
 end
