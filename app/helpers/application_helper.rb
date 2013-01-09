1401a593475cef58653cdccb65696b221a9d7d80
Shay Davidson <shay.h.davidson@gmail.com>
2013-01-09 17:17:00 +0200

    some helpers orders

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,3 @@
 module ApplicationHelper
 
-  def google_maps_api_tag
-    javascript_include_tag 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDnMkrIGwuDCrXC50JoVRc7ogYq5STh3BQ&sensor=true'
-  end
 end
