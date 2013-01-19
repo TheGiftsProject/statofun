cf6a999f62508c05477f80cc5558197f683b0b69
Shay Davidson <shay.h.davidson@gmail.com>
2013-01-19 18:26:09 +0200

    thank god

--- app/helpers/google_helper.rb
+++ app/helpers/google_helper.rb
@@ -1,7 +1,23 @@
 module GoogleHelper
 
   def google_maps_api_tag
-    javascript_include_tag "https://maps.googleapis.com/maps/api/js?key=#{GOOGLE.api_key}&sensor=true"
+    params = {
+        :key    => GOOGLE.api_key,
+        :sensor => true
+    }
+    javascript_include_tag "https://maps.googleapis.com/maps/api/js?#{params.to_query}"
+  end
+
+  def google_static_map(ltd, lng, zoom = 14, width = 640, height = 640)
+    params = {
+        :key    => GOOGLE.api_key,
+        :sensor => false,
+        :zoom   => zoom,
+        :center => true,
+        :size   => "#{width}x#{height}",
+        :markers => "color:green|#{ltd},#{lng}"
+    }
+    "http://maps.googleapis.com/maps/api/staticmap?center=#{ltd},#{lng}&#{params.to_query}"
   end
 
 end
