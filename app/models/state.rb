645d0cb82ed7e9c39443d73f07522df3d1075c0d
Shay Davidson <shay.h.davidson@gmail.com>
2013-01-23 17:48:00 +0200

    stations shize

--- app/models/state.rb
+++ app/models/state.rb
@@ -4,7 +4,12 @@ class State < ActiveRecord::Base
 
   attr_accessible :active, :available_bikes, :available_docks
 
+  scope :at_day, lambda { |date| where(:created_at => date.beginning_of_day..date.end_of_day) }
+  scope :between_dates, lambda { |date0, date1| where(:created_at => date0.beginning_of_day..date1.end_of_day)}
+
   validates_numericality_of :available_bikes, :greater_than_or_equal_to => 0
   validates_numericality_of :available_docks, :greater_than_or_equal_to => 0
+
+
 
 end
--- app/models/station.rb
+++ app/models/station.rb
@@ -4,8 +4,34 @@ class Station < ActiveRecord::Base
 
   attr_accessible :sid, :he_name, :en_name, :he_desc, :en_desc, :lng, :ltd
 
-  def states_at_day(date = Time.now)
-    states.where(:created_at => date.beginning_of_day..date.end_of_day).map(&:available_bikes)
+
+  def self.all_transactions_at_dates(date0 = 1.week.ago, date1 = Time.now)
+    all
+  end
+
+  def transactions_at_dates(date0 = 1.week.ago, date1 = Time.now)
+    taken    = 0
+    returned = 0
+
+    current_bike_count = nil
+    states.between_dates(date0, date1).each do |state|
+
+      new_count = state.available_bikes
+
+      if current_bike_count.present?
+        if new_count > current_bike_count
+          returned += new_count - current_bike_count
+        elsif new_count < current_bike_count
+          taken += current_bike_count - new_count
+        end
+      end
+
+      current_bike_count = new_count
+
+    end
+
+    {:taken => taken, :returned => returned}
+
   end
 
 end
